09d3c3cc751bb7df8454ed828399b5f49abf2a68
ahmed <ahmadinejad.am50@gmail.com>
2024-01-14 15:14:14 +0200

    update bastion module

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "bastion-server" {
   ami             = "ami-027a754129abb5386"
-  instance_type   = "m2.micro"
+  instance_type   = "t2.micro"
   key_name        = var.ssh-key
+  subnet_id = var.subnet-id
   security_groups = var.security-groups
 
   tags = {
--- terraform/modules/bastion/vars.tf
+++ terraform/modules/bastion/vars.tf
@@ -2,6 +2,10 @@ variable "ssh-key" {
   type = string
 }
 
+variable "subnet-id" {
+  type = string
+}
+
 variable "security-groups" {
   type = list(string)
 }
