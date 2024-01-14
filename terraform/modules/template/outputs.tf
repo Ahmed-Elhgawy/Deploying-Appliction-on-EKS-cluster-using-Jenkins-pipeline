62a16919ff976ceb289b13f20e1c238384d26fe9
ahmed <ahmadinejad.am50@gmail.com>
2024-01-14 18:39:05 +0200

    delete

--- terraform/modules/template/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "template_name" {
-  value = aws_launch_template.k8s_template.name
-}
-
-output "template_version" {
-  value = aws_launch_template.k8s_template.latest_version
-}
--- terraform/modules/template/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "security-groups-id" {
-  type = list(string)
-}
-
-variable "ssh-key" {
-  type = string
-}
